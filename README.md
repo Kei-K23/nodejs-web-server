87869231cf45e5c858b837a7a0919063484f4942
Kei-K23 <keiksl2301@gmail.com>
2024-08-13 15:47:11 +0630

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# NodeJS Web Server
+
+Build NodeJS web server from scratch with TypeScript.
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,32 @@
-import * as net from "net"
+import * as net from "net";
 
 function newConn(socket: net.Socket) {
-    console.log("new connection : ", socket.remoteAddress, socket.remotePort);
+  console.log("new connection : ", socket.remoteAddress, socket.remotePort);
+
+  // Adds the listener for client Socket connection
+  socket.on("data", (data: Buffer) => {
+    socket.write(data);
+
+    if (data.includes("q")) {
+      socket.end();
+    }
+  });
+
+  socket.on("end", () => {
+    console.log("EOF");
+  });
 }
 
 // Creates a new TCP Socket server
-let server = net.createServer();
+let server = net.createServer({ allowHalfOpen: true });
 
-// Adds the listener for Socket connection
-server.on('connection', newConn);
-server.on('error', (err: Error) => { throw err });
+// Adds the listener for client Socket connection
+server.on("connection", newConn);
+
+// Adds the listener for error connection
+server.on("error", (err: Error) => {
+  throw err;
+});
+
 // Start a server listening for connections
-server.listen(4321, '127.0.0.1');
+server.listen(4321, "127.0.0.1");
